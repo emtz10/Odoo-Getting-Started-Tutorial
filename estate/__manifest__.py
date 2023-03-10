970a209d3398e516c5ee2541d796827761db761c
Eduardo Mtz <eduardoms@vauxoo.com>
2023-03-10 13:06:54 -0600

    [ADD] real estate module: create new odoo module
    This adds all changes until Chapter 6 of the Getting Started tutorial
    from Odoo in order to create  basic functionality to the real estate
    module.

---
+++ estate/__manifest__.py
@@ -0,0 +1,25 @@
+{
+    'name': "Real Estate",
+    'version': '1.0',
+    'depends': ['base'],
+    'author': "Eduardo",
+    'installable': True,
+    'application': True,
+    'data': [
+        'security/ir.model.access.csv',
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml',
+    ],
+    # 'category': 'Category',
+    # 'description': """
+    # Description text
+    # """,
+    # # data files always loaded at installation
+    # 'data': [
+    #     'views/mymodule_view.xml',
+    # ],
+    # # data files containing optionally loaded demonstration data
+    # 'demo': [
+    #     'demo/demo_data.xml',
+    # ],
+}
---
+++ estate/models/estate_property.py
@@ -0,0 +1,39 @@
+"""" Real Estate Property module for Odoo 16 """
+
+"""" Import packages from Odoo """
+from odoo import fields, models
+"""" Import date and time libraries """
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+
+class EstateProperty(models.Model):
+    """" Set name to table in database, `.` converts to `_` """
+    _name = "estate.property"
+    _description = "Real Estate Property Data"
+
+    name = fields.Char(required=True)
+    description = fields.Char(required=True)
+    postcode = fields.Char(required=True)
+    """" Relativedelta allows to add time by months because datetime only allows by days """
+    date_availability = fields.Date('Date Availability', default=datetime.now() + relativedelta(months=3), copy=False)
+    expected_price = fields.Float('Expected price', digits=(10,1), required=True)
+    """" 'Copy' parameter defines if the value of the field can be copied when duplicated """
+    selling_price = fields.Float('Selling price', digits=(10,1), readonly=True, copy=False)
+    bedrooms = fields.Integer(default=2)
+    living_area = fields.Integer('Living area')
+    facades = fields.Integer()
+    garage = fields.Boolean(default=False)
+    garden = fields.Boolean(default=False)
+    garden_area = fields.Integer('Garden area')
+    garden_orientation =  fields.Selection(string='Garden orientation', selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')])
+    """" Adding reserved fields to table """
+    active = fields.Boolean(default=True)
+    state = fields.Selection(
+        string='Status',
+        selection=([
+            ('new', 'New'),
+            ('offer_received', 'Offer Received'),
+            ('offer_accepted', 'Offer Accepted'),
+            ('sold_and_canceled', 'Sold and Canceled')
+        ]), required=True, copy=False, default='new'
+    )
