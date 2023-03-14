66021b9ebe5cc4e217a993284aa55c0888cb14d2
Eduardo Mtz <eduardoms@vauxoo.com>
2023-03-13 22:41:20 -0600

    [IMP] real estate module: add computed fields
    This automates filling the form data as some fields update their values
    depeneding on anothers so it makes registration for the user easier.

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,7 +1,7 @@
 """" Real Estate Property module for Odoo 16 """
 
 """" Import packages from Odoo """
-from odoo import fields, models
+from odoo import api, fields, models
 """" Import date and time libraries """
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
@@ -30,7 +30,9 @@ class EstateProperty(models.Model):
     users_id = fields.Many2one('res.users', string='Salesman', default=lambda self: self.env.user)
     partner_id = fields.Many2one('res.partner', string='Buyer', copy=False)
     property_tag_id = fields.Many2many('estate.property.tag', string="Tags")
-    offer_ids = fields.One2many("estate.property.offer", "property_id", string="Offers")
+    offer_id = fields.One2many("estate.property.offer", "property_id", string="Offers")
+    total_area = fields.Integer(compute='_compute_total_area')
+    best_price = fields.Float(compute='_compute_best_offer')
     """" Adding reserved fields to table """
     active = fields.Boolean(default=True)
     state = fields.Selection(
@@ -42,6 +44,29 @@ class EstateProperty(models.Model):
             ('sold_and_canceled', 'Sold and Canceled')
         ]), required=True, copy=False, default='new'
     )
+
+    # Computed fields
+    # Adding dependencies to decorator
+    @api.depends("living_area", "garden_area")
+    def _compute_total_area(self):
+        for record in self:
+            # Updating field with sum of values
+            record.total_area = record.living_area + record.garden_area
+
+    @api.depends("offer_id.price")
+    def _compute_best_offer(self):
+        for offer in self:
+            offer.best_price = max(self.mapped('offer_id.price')) if self.mapped('offer_id.price') else 0
+
+    # Onchange method to update field values when another field is updated
+    def _onchange_garden(self):
+        if self.garden:
+            self.garden_area = 10
+            self.garden_orientation = 'north'
+        else:
+            self.garden_area = 0
+            self.garden_orientation = ''
+
 
 
 class EstatePropertyType(models.Model):
@@ -66,3 +91,16 @@ class EstatePropertyOffer(models.Model):
     status = fields.Selection(selection=[('accepted', 'Accepted'), ('refused', 'Refused')], copy=False)
     partner_id = fields.Many2one('res.partner', required=True)
     property_id = fields.Many2one('estate.property', required=True)
+    validity = fields.Integer(default=7, string="Validity (days)")
+    date_deadline = fields.Date(compute='_compute_deadline', inverse='_inverse_deadline',  store=True)
+
+    @api.depends("validity", "create_date")
+    def _compute_deadline(self):
+        for record in self:
+            record.date_deadline = (record.create_date if record.create_date else datetime.now().date()) + relativedelta(days=record.validity)
+
+    # This method works similar as the compute method as it update the field values based on another field
+    # This method only works after save a record
+    def _inverse_deadline(self):
+        for record in self:
+            record.validity = relativedelta(record.date_deadline, record.create_date.date()).days
